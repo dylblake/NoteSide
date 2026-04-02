2262e8553e7969a8cb33bea8a9d011afd84537aa
Dylan Evans <dylblake03@gmail.com>
2026-04-02 15:52:30 -0400

    Initial Commit

---
+++ side-note/side_noteApp.swift
@@ -0,0 +1,17 @@
+//
+//  side_noteApp.swift
+//  side-note
+//
+//  Created by Dylan Evans on 4/2/26.
+//
+
+import SwiftUI
+
+@main
+struct side_noteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
